a1f1f7c03014418b20c3eba9ecf4988a333cf4c2
ivorking <ivorking@gmail.com>
2018-10-09 16:15:04 +0100

    update misc changes

--- routes/web.php
+++ routes/web.php
@@ -11,21 +11,23 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::get('/muggins', function () {
-    return "here's something";
-});
+// Route::get('/muggins', function () {
+//     return "here's something";
+// });
 
-Route::get('/post/{id}/{name}', function($id, $name) {
-    return "this is post". $id . " " . $name;
-});
+// Route::get('/post/{id}/{name}', function($id, $name) {
+//     return "this is post". $id . " " . $name;
+// });
 
-Route::get('admin/posts/example', array('as'=>'admin.home',function(){
-    $url = route('admin.home');
-    return "this url is ". $url;
-}));
+// Route::get('admin/posts/example', array('as'=>'admin.home',function(){
+//     $url = route('admin.home');
+//     return "this url is ". $url;
+// }));
 
+
+Route::get('/post/{id}', 'PostsController@index');
 
